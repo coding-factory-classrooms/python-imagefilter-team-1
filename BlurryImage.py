7a28d17a74523d0a6b965e2f402db1467c5128c7
Léo <leo.gregori95@gmail.com>
2020-12-16 11:52:06 +0100

    Fix error for filters

--- BlurryImage.py
+++ BlurryImage.py
@@ -1,7 +1,11 @@
 import cv2
 
 def blur_filter():
-    image = cv2.imread('imgs/8-Mile.jpg')
-    blurImage = cv2.GaussianBlur(image, (15, 15), 0)
+    try:
+        image = cv2.imread('imgs/8-Mile.jpg')
+        blurImage = cv2.GaussianBlur(image, (15, 15), 0)
+        cv2.imwrite('blur_imgs/8-Mile.jpg', blurImage)
+    except cv2.error as e:
+        print(e)
 
-    cv2.imwrite('blur_imgs/8-Mile.jpg', blurImage)
+
--- DilatedImage.py
+++ DilatedImage.py
@@ -3,11 +3,16 @@ import numpy
 
 def dilated_image():
 
-    image = cv2.imread('imgs/8-Mile.jpg')
-    newImg = cv2.split(image)[0]
-    (retVal, newImg) = cv2.threshold(newImg, 130, 255, cv2.THRESH_BINARY)
-    kernel = numpy.ones((6, 6), numpy.uint8)
-    newImg = cv2.dilate(newImg, kernel, iterations=1)
+    try:
+        image = cv2.imread('imgs/8-Mile.jpg')
+        newImg = cv2.split(image)[0]
+        (retVal, newImg) = cv2.threshold(newImg, 130, 255, cv2.THRESH_BINARY)
+        kernel = numpy.ones((6, 6), numpy.uint8)
+        newImg = cv2.dilate(newImg, kernel, iterations=1)
+        cv2.imwrite('dilated_imgs/8-Mile.jpg', newImg)
+    except cv2.error as e:
+        print(e)
+    except IndexError as e:
+        print(e)
 
-    cv2.imwrite('dilated_imgs/8-Mile.jpg', newImg)
 
