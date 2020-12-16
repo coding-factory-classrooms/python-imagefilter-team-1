8d4c7a085bfd3ae50b824f5d96b829e9c65b68f1
Drakkarr <72389960+Drakkarr@users.noreply.github.com>
2020-12-16 14:17:29 +0100

    ajout fonction All_images + image mise en paramètre

--- BlurryImage.py
+++ BlurryImage.py
@@ -3,5 +3,5 @@ import cv2
 def blur_filter():
     image = cv2.imread('imgs/8-Mile.jpg')
     blurImage = cv2.GaussianBlur(image, (15, 15), 0)
-
+    cv2.cvtColor(blurImage, )
     cv2.imwrite('blur_imgs/8-Mile.jpg', blurImage)
